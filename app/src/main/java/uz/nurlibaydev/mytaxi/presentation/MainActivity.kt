2d857d193fa2ca30bcae8ba3d68d58e77debd744
Nurlibay <koshkinbayevn@gmail.com>
2022-11-22 16:31:58 +0500

    screens created

--- app/src/main/java/uz/nurlibaydev/mytaxi/presentation/MainActivity.kt
+++ app/src/main/java/uz/nurlibaydev/mytaxi/presentation/MainActivity.kt
@@ -1,12 +1,6 @@
 package uz.nurlibaydev.mytaxi.presentation
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import uz.nurlibaydev.mytaxi.R
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
