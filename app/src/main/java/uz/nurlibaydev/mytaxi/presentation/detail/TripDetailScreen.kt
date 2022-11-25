601fc0ce51f55b5e36557a175bff44c84e72e2d8
Nurlibay <koshkinbayevn@gmail.com>
2022-11-26 01:41:37 +0500

    trip detail being prepared

--- app/src/main/java/uz/nurlibaydev/mytaxi/presentation/detail/TripDetailScreen.kt
+++ app/src/main/java/uz/nurlibaydev/mytaxi/presentation/detail/TripDetailScreen.kt
@@ -2,10 +2,12 @@ package uz.nurlibaydev.mytaxi.presentation.detail
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import uz.nurlibaydev.mytaxi.R
 import uz.nurlibaydev.mytaxi.databinding.ScreenTripDetailBinding
 import uz.nurlibaydev.mytaxi.presentation.map.SupportMapScreen
@@ -19,6 +21,7 @@ class TripDetailScreen: Fragment(R.layout.screen_trip_detail) {
 
     private val binding: ScreenTripDetailBinding by viewBinding()
     private lateinit var googleMap: GoogleMap
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<NestedScrollView>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -35,6 +38,9 @@ class TripDetailScreen: Fragment(R.layout.screen_trip_detail) {
                     isCompassEnabled = false
                 }
             }
+            bottomSheetBehavior = BottomSheetBehavior.from(nestedScrollView)
+            val peekHeight = (resources.displayMetrics.heightPixels * 0.6).toInt()
+            bottomSheetBehavior.peekHeight = peekHeight
         }
     }
 }
