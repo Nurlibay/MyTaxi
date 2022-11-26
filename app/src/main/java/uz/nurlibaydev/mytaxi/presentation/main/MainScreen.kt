005087e176c01f2d1dfd46f10ea278a273e13d2a
Nurlibay <koshkinbayevn@gmail.com>
2022-11-26 19:48:05 +0500

    some ui errors fixed

--- app/src/main/java/uz/nurlibaydev/mytaxi/presentation/main/MainScreen.kt
+++ app/src/main/java/uz/nurlibaydev/mytaxi/presentation/main/MainScreen.kt
@@ -1,5 +1,6 @@
 package uz.nurlibaydev.mytaxi.presentation.main
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.*
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -11,44 +12,43 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import com.google.android.material.navigation.NavigationView
 import uz.nurlibaydev.mytaxi.R
 import uz.nurlibaydev.mytaxi.databinding.ScreenMainBinding
-import uz.nurlibaydev.mytaxi.utils.AdvanceDrawerLayout
-
-/**
- *  Created by Nurlibay Koshkinbaev on 22/11/2022 16:07
- */
 
 class MainScreen : Fragment(R.layout.screen_main), NavigationView.OnNavigationItemSelectedListener {
 
     private val binding: ScreenMainBinding by viewBinding()
     private lateinit var navController: NavController
 
-    private lateinit var drawer: AdvanceDrawerLayout
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        drawer = binding.drawerLayout
-        val toolbar = binding.appBar.toolbar
-        navController = Navigation.findNavController(requireActivity(), R.id.fragmentContainerView)
-        val toggle = ActionBarDrawerToggle(requireActivity(), drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        toggle.isDrawerIndicatorEnabled = false
-        toggle.setHomeAsUpIndicator(R.drawable.ic_menu)
-        drawer.addDrawerListener(toggle)
-        toggle.syncState()
-        binding.navView.setNavigationItemSelectedListener(this)
-        toolbar.setNavigationOnClickListener {
-            drawer.openDrawer(GravityCompat.START)
+        binding.apply {
+            val toolbar = mainScreenContent.toolbar
+            mainScreenContent.appBar.setBackgroundColor(Color.TRANSPARENT)
+            navController = Navigation.findNavController(requireActivity(), R.id.fragmentContainerView)
+            val toggle = ActionBarDrawerToggle(requireActivity(), drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+
+            toggle.isDrawerIndicatorEnabled = false
+            toggle.setHomeAsUpIndicator(R.drawable.ic_menu)
+            drawerLayout.addDrawerListener(toggle)
+            toggle.syncState()
+            navigationView.setNavigationItemSelectedListener(this@MainScreen)
+            drawerLayout.setRadius(Gravity.START, 35f)
+            drawerLayout.setViewScale(Gravity.START, 0.9f)
+            drawerLayout.setViewElevation(Gravity.START, 20f)
+
+            toolbar.setNavigationOnClickListener {
+                drawerLayout.openDrawer(GravityCompat.START)
+            }
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.main_drawer, menu)
-        return super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_drawer, menu)
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         return when(item.itemId){
             R.id.tripHistoryScreen -> {
-                drawer.closeDrawer(GravityCompat.START)
+                binding.drawerLayout.closeDrawer(GravityCompat.START)
                 navController.navigate(MainScreenDirections.actionMainScreenToTripHistoryScreen())
                 true
             }
