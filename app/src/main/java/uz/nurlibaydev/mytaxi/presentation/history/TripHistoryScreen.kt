8274fa9c149a0d5983810cb1ea6ff914874a53df
Nurlibay <koshkinbayevn@gmail.com>
2022-11-27 15:43:42 +0500

    shimmer effect ready!

--- app/src/main/java/uz/nurlibaydev/mytaxi/presentation/history/TripHistoryScreen.kt
+++ app/src/main/java/uz/nurlibaydev/mytaxi/presentation/history/TripHistoryScreen.kt
@@ -1,6 +1,8 @@
 package uz.nurlibaydev.mytaxi.presentation.history
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
@@ -10,15 +12,12 @@ import uz.nurlibaydev.mytaxi.R
 import uz.nurlibaydev.mytaxi.data.source.LocalData
 import uz.nurlibaydev.mytaxi.databinding.ScreenTripHistoryBinding
 
-/**
- *  Created by Nurlibay Koshkinbaev on 22/11/2022 16:09
- */
-
-class TripHistoryScreen: Fragment(R.layout.screen_trip_history) {
+class TripHistoryScreen : Fragment(R.layout.screen_trip_history) {
 
     private val binding: ScreenTripHistoryBinding by viewBinding()
     private val adapter by lazy { TripHistoryAdapter() }
     private val navController: NavController by lazy(LazyThreadSafetyMode.NONE) { findNavController() }
+    private var isCreated = false
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -27,6 +26,18 @@ class TripHistoryScreen: Fragment(R.layout.screen_trip_history) {
                 findNavController().popBackStack()
             }
             rvTripHistory.adapter = adapter
+            if(!isCreated){
+                shimmerLayout.startShimmer()
+                Handler(Looper.getMainLooper()).postDelayed({
+                    shimmerLayout.visibility = View.INVISIBLE
+                    rvTripHistory.visibility = View.VISIBLE
+                }, 4000)
+                isCreated = true
+            } else {
+                shimmerLayout.stopShimmer()
+                shimmerLayout.visibility = View.INVISIBLE
+                rvTripHistory.visibility = View.VISIBLE
+            }
             adapter.submitList(LocalData.tripHistoryList)
             adapter.setOnItemClickListener {
                 navController.navigate(TripHistoryScreenDirections.actionTripHistoryScreenToTripDetailScreen(it.destinationData))
